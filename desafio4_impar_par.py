9e087abe8fb2fc477ad00e72163fb4e3247b1bd6
noemipaiva <noemi.santos001@gmail.com>
2025-09-19 15:51:50 -0300

    Adicionado o desafio4_impar_par.py

---
+++ desafio4_impar_par.py
@@ -0,0 +1,11 @@
+
+# desafio4_impar_par.py
+
+#Solicita um número inteiro ao usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se é par ou ímpar
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR.")
+else:
+    print(f"O número {numero} é ÍMPAR.")
