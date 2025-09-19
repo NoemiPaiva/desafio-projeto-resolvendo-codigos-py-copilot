bc7e6c9bb2ec1ec53f60f8c4db083a7f33e1660c
noemipaiva <noemi.santos001@gmail.com>
2025-09-19 15:47:09 -0300

    Adicionado o desafio3_operacoes_mat.py

---
+++ desafio3_operacoes_mat.py
@@ -0,0 +1,12 @@
+# desafio3_operacoes_mat.py
+
+# Solicita dois números ao usuário
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+# Realiza as operações
+print("Resultados das operações:")
+print(f"Adição: {a} + {b} = {a + b}")
+print(f"Subtração: {a} - {b} = {a - b}")
+print(f"Multiplicação: {a} * {b} = {a * b}")
+print(f"Divisão: {a} / {b} = {a / b}")
